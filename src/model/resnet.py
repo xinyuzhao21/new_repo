c70b4a4de3d013aef4e11738df2c4c8f43a9d6dc
xinyuzhao21 <xinyuzhao@umass.edu>
2020-11-16 09:50:15 -0500

    added resnet

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,11 +1,15 @@
 import torchvision.models as models
 import torch.nn as nn
-def getResnet(num_class = 125, pretrain=False):
+def getResnet(num_class = 125, embed_size=-1, pretrain=False):
     model = models.resnet18(pretrained=pretrain)
     # for name, child in model.named_children():
     #     for name2, params in child.named_parameters():
     #         print(name, name2)
-    model.fc = nn.Linear(model.fc.in_features,num_class)
+    if embed_size <0:
+        model.fc = nn.Linear(model.fc.in_features,num_class)
+    else:
+        model.fc = nn.Sequential(
+            nn.Linear(model.fc.in_features,embed_size),
+            nn.Linear(embed_size,num_class)
+        )
     return model
-
-model = getResnet()
